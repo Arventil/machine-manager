6f1a2daac184c72489eafa2adb67557036cd91ec
Arventil <pawel.merek92@gmail.com>
2019-04-24 18:52:13 +0200

    Add-machine form changed a bit (textarea for adding new types of handling added)

--- public/js/addMachineForm.js
+++ public/js/addMachineForm.js
@@ -1,9 +1,28 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     let ifIICheck = document.querySelector('#ifII');
+    let ifDHCheck = document.querySelector('#ifDH');
+    let ifWHCheck = document.querySelector('#ifWH');
+    let ifMHCheck = document.querySelector('#ifMH');
+    let ifQHCheck = document.querySelector('#ifQH');
+    let ifHYHCheck = document.querySelector('#ifHYH');
+    let ifYHCheck = document.querySelector('#ifYH');
+
     let iDateInputs = document.querySelectorAll('.iDate');
+    let DHInputs = document.querySelector('.DH');
+    let WHInputs = document.querySelector('.WH');
+    let MHInputs = document.querySelector('.MH');
+    let QHInputs = document.querySelector('.QH');
+    let HYHInputs = document.querySelector('.HYH');
+    let YHInputs = document.querySelector('.YH');
 
     ifIICheck.addEventListener('change', showIDataInputs);
+    ifDHCheck.addEventListener('change', showDHInputs);
+    ifWHCheck.addEventListener('change', showWHInputs);
+    ifMHCheck.addEventListener('change', showMHInputs);
+    ifQHCheck.addEventListener('change', showQHInputs);
+    ifHYHCheck.addEventListener('change', showHYHInputs);
+    ifYHCheck.addEventListener('change', showYHInputs);
 
     function showIDataInputs(){
 
@@ -17,5 +36,65 @@ document.addEventListener('DOMContentLoaded', function(){
                 iDateInputs[a].style.display = 'none';
             }
         }
-    }    
+    }   
+    
+    function showDHInputs(){
+
+        if(ifDHCheck.checked == true){
+            DHInputs.style.display = 'block';
+        }
+        else {
+            DHInputs.style.display = 'none';
+        }
+    } 
+
+    function showWHInputs(){
+
+        if(ifWHCheck.checked == true){
+            WHInputs.style.display = 'block';
+        }
+        else {
+            WHInputs.style.display = 'none';
+        }
+    } 
+    
+    function showMHInputs(){
+
+        if(ifMHCheck.checked == true){
+            MHInputs.style.display = 'block';
+        }
+        else {
+            MHInputs.style.display = 'none';
+        }
+    } 
+
+    function showQHInputs(){
+
+        if(ifQHCheck.checked == true){
+            QHInputs.style.display = 'block';
+        }
+        else {
+            QHInputs.style.display = 'none';
+        }
+    } 
+
+    function showHYHInputs(){
+
+        if(ifHYHCheck.checked == true){
+            HYHInputs.style.display = 'block';
+        }
+        else {
+            HYHInputs.style.display = 'none';
+        }
+    } 
+
+    function showYHInputs(){
+
+        if(ifHYHCheck.checked == true){
+            YHInputs.style.display = 'block';
+        }
+        else {
+            YHInputs.style.display = 'none';
+        }
+    } 
 });
