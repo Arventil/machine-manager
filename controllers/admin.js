8f9b0c857c8982c0eb4dc493b86ffebcf258ac11
Arventil <pawel.merek92@gmail.com>
2019-04-28 12:28:04 +0200

    Mechanism for adding machine and handlings for that machine into database added

--- controllers/admin.js
+++ controllers/admin.js
@@ -23,31 +23,54 @@ exports.getAddMachine = (req, res, next) =>{
 }
 
 exports.postAddMachine = (req, res, next) =>{
-    let dHString = req.body.dailyHand.toString();
-    let dHTable = [];
-    let dHRecord = '';
-    for(let a = 0; a < dHString.length; a++){
-        if(dHString[a] != ','){
-            dHRecord = dHRecord + dHString[a];
+    let machineName = req.body.name;
+    let inspectionDate = req.body.inspectionDate;
+    let insuranceDate = req.body.insuranceDate;
+    let dHString = req.body.dailyHand;
+    let dHTable = insertDailyHandsIntoTable(dHString);
+    let wHString = req.body.weeklyHand;
+    let wHTable = insertDailyHandsIntoTable(wHString);
+    let mHString = req.body.monthlyHand;
+    let mHTable = insertDailyHandsIntoTable(mHString);
+    let qHString = req.body.quartalyHand;
+    let qHTable = insertDailyHandsIntoTable(qHString);
+    let hYHString = req.body.halfYearlyHand;
+    let hYHTable = insertDailyHandsIntoTable(hYHString);
+    let yHString = req.body.yearlyHand;
+    let yHTable = insertDailyHandsIntoTable(yHString);
+    let machine = new Machine(machineName, inspectionDate, insuranceDate, dHTable, wHTable, mHTable, qHTable, hYHTable, yHTable);
+    machine.save();
+    res.redirect('/admin/addMachine');
+}
+
+
+// Pobiera dane do danej osbługi (np. dziennej) z przekazanego do funkcji parametru - czyste dane z req.body dotyczące odpowiedniej obsługi, 
+// a następnie dzieli je na podstawie przecinków, usuwa znaki "enteru" i wrzuca je do tablicy, którą zwraca, uprzednio przerabiając ją na string za pomocą JSON.
+function insertDailyHandsIntoTable(bodyHandString){
+    let hTable = [];
+    let hRecord = '';
+    for(let a = 0; a < bodyHandString.length; a++){
+        if(bodyHandString[a] != ','){
+            hRecord = hRecord + bodyHandString[a];
         }
         else{
-            if(dHTable.length == 0){
-                dHTable.push(dHRecord);
-                dHRecord = '';
+            if(hTable.length == 0){
+                hTable.push(hRecord);
+                hRecord = '';
             }
             else{
-                dHRecord = dHRecord.substring(2, dHRecord.length);
-                dHTable.push(dHRecord);
-                dHRecord = '';
+                hRecord = hRecord.substring(2, hRecord.length);
+                hTable.push(hRecord);
+                hRecord = '';
             }
         }
-        if(a == dHString.length - 1)
+        if(a == bodyHandString.length - 1)
         {
-            dHRecord = dHRecord.substring(2, dHRecord.length);
-            dHTable.push(dHRecord);
-            dHRecord = '';
+            hRecord = hRecord.substring(2, hRecord.length);
+            hTable.push(hRecord);
+            hRecord = '';
         }
     }
-    console.log(dHTable);
-    res.redirect('/admin/addMachine');
+
+    return JSON.stringify(hTable);
 }
--- models/machine.js
+++ models/machine.js
@@ -1,15 +1,20 @@
 const db = require('../util/database');
 
 module.exports = class Machine {
-    constructor(name, ifInspection, ifInsurance, inspectionDate, insuranceDate) {
-        this.name = name;
-        this.ifInspection = ifInspection;
-        this.ifInsurance = ifInsurance;        
+    constructor(name, inspectionDate, insuranceDate, dailyHand, weeklyHand, monthlyHand, quartalyHand, halfYearlyHand, yearlyHand) {
+        this.name = name;      
         this.inspectionDate = inspectionDate;
         this.insuranceDate = insuranceDate;
+        this.dailyHand = dailyHand;
+        this.weeklyHand = weeklyHand;
+        this.monthlyHand = monthlyHand;
+        this.quartalyHand = quartalyHand;
+        this.halfYearlyHand = halfYearlyHand;
+        this.yearlyHand = yearlyHand;
+        this.handStatus = null;
     }
     save() {
-    
+        db.execute('INSERT INTO machines (name, inspectionDate, insuranceDate, dailyHand, weeklyHand, monthlyHand, quartalyHand, halfYearlyHand, yearlyHand, handStatus) VALUES (?,?,?,?,?,?,?,?,?,?)', [this.name, this.inspectionDate, this.insuranceDate, this.dailyHand, this.weeklyHand, this.monthlyHand, this.quartalyHand, this.halfYearlyHand, this.yearlyHand, this.handStatus]);
     }
 
     static deleteById(id){
